cd329fe17190858e7cb2fd018f4408f99ad33f85
Pavel Kozlov <pavel.mno4@gmail.com>
2024-10-31 08:45:03 +0300

    initial commit

---
+++ src/test/java/ru/pkozlov/app/TestAppApplication.java
@@ -0,0 +1,11 @@
+package ru.pkozlov.app;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AppApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
