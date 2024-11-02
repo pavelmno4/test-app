fc874b308c642411e5814cffc18c903c85259258
Pavel Kozlov <pavel.mno4@gmail.com>
2024-11-02 22:53:15 +0300

    add ControllerExceptionHandlerConfig

--- src/main/java/ru/pkozlov/app/api/config/ControllerExceptionHandlerConfig.java
+++ src/main/java/ru/pkozlov/app/api/config/ControllerExceptionHandlerConfig.java
@@ -1,4 +1,35 @@
 package ru.pkozlov.app.api.config;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.pkozlov.app.api.dto.ErrorDto;
+import ru.pkozlov.app.service.exception.NotFoundException;
+import ru.pkozlov.app.service.exception.ValidationException;
+
+@ControllerAdvice
 public class ControllerExceptionHandlerConfig {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorDto> handleValidationException(ValidationException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorDto(exception.getMessage()));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDto> handleNotFoundException(NotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorDto(exception.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorDto> handleBadCredentialsException(BadCredentialsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorDto(exception.getMessage()));
+    }
 }
---
+++ src/main/java/ru/pkozlov/app/api/dto/ErrorDto.java
@@ -0,0 +1,10 @@
+package ru.pkozlov.app.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorDto {
+    private String message;
+}
